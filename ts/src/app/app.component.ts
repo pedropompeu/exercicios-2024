041aab298e66c54e6d8a2a0af7ada3caab5c1d4c
pedropompeu <pedrolpompeu@gmail.com>
2024-05-13 22:05:10 -0300

    Construção do menu lateral e do header

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'DevChuva';
+  title: string = 'DevChuva';
+  show = false
+
+  onToggleShow = () => {
+    this.show = !this.show
+  }
 }
